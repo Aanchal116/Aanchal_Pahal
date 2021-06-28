a680d07564bc970ddcd4beaf5e5c310473c50bfb
Aanchal116 <48170750+Aanchal116@users.noreply.github.com>
2021-06-29 01:55:51 +0530

    Fixed the contact form

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.backgroundColor = "#000";
+
+    document.getElementById("navbar").style.backgroundColor = "black";
     var element = document.getElementById("about-div");
     element.classList.add("slide-right");
-    
-  } else{
+  } 
+  else{
     document.getElementById("navbar").style.backgroundColor = "transparent";
   }
 
 
 }
 
-function scrollFun(){
-  if(document.documentElement.scrollTop > 80){
-    var element = document.getElementById("about-div");
-    element.classList.add("slide-right");
-  }
-  
-}
 
